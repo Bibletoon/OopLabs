65e0a87099910801f23aa95beaa52372a6a4848f
Bibletoon <alex8.ivanov@gmail.com>
2021-11-20 09:03:24 +0300

    Refactor: DRY in configuration manager

--- BackupsExtra/ConfigurationManager.cs
+++ BackupsExtra/ConfigurationManager.cs
@@ -12,15 +12,6 @@ namespace BackupsExtra
 {
     public class ConfigurationManager
     {
-        private static readonly Lazy<ConfigurationManager> _instance =
-            new Lazy<ConfigurationManager>(() => new ConfigurationManager());
-
-        private ConfigurationManager()
-        {
-        }
-
-        public static ConfigurationManager Instance => _instance.Value;
-
         public void Save(JobConfiguration configuration, string filename)
         {
             ArgumentNullException.ThrowIfNull(configuration, nameof(configuration));
@@ -31,11 +22,21 @@ namespace BackupsExtra
 
         public BackupJob LoadBackupJob(TypeLocator typeLocator, string filename)
         {
+            var serviceCollection = new ServiceCollection();
+
+            LoadJobServices(typeLocator, filename, serviceCollection);
+
+            serviceCollection.AddSingleton<BackupJob>();
+            return serviceCollection.BuildServiceProvider().GetService<BackupJob>();
+        }
+
+        internal void LoadJobServices(TypeLocator typeLocator, string filename, ServiceCollection serviceCollection)
+        {
             ArgumentNullException.ThrowIfNull(typeLocator, nameof(typeLocator));
+            ArgumentNullException.ThrowIfNull(serviceCollection, nameof(serviceCollection));
             if (!File.Exists(filename))
                 throw new FileNotFoundException("No such configuration file");
             var stringConfiguration = File.ReadAllText(filename);
-
             JobConfiguration configuration;
 
             try
@@ -46,35 +47,6 @@ namespace BackupsExtra
             {
                 throw new JobConfigurationException("Invalid configuration file", e);
             }
-
-            var serviceCollection = new ServiceCollection();
-
-            foreach (var service in configuration.ServicesConfiguration.Services)
-            {
-                var type = typeLocator.GetType(service.Type);
-                var implementationType = typeLocator.GetType(service.ImplementationType);
-                serviceCollection.AddScoped(type, implementationType);
-            }
-
-            foreach (var serviceConfiguration in configuration.ServicesConfiguration.ServicesConfigurations)
-            {
-                var type = typeLocator.GetType(serviceConfiguration.Type);
-                var deserializedConfig = serviceConfiguration.ConfigurationObject as JObject;
-                serviceCollection.AddSingleton(type, deserializedConfig.ToObject(type));
-            }
-
-            serviceCollection.AddSingleton(configuration);
-            serviceCollection.AddSingleton<BackupJob>();
-            return serviceCollection.BuildServiceProvider().GetService<BackupJob>();
-        }
-
-        internal void LoadJobServices(TypeLocator typeLocator, string filename, ServiceCollection serviceCollection)
-        {
-            ArgumentNullException.ThrowIfNull(typeLocator, nameof(typeLocator));
-            if (!File.Exists(filename))
-                throw new FileNotFoundException("No such configuration file");
-            var stringConfiguration = File.ReadAllText(filename);
-            var configuration = JsonConvert.DeserializeObject<JobConfiguration>(stringConfiguration);
 
             if (configuration is null)
                 throw new JobConfigurationException("Invalid configuration file");
